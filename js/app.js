6667bf4d542f4121d85fa3ea0fd9652c6ef1484e
Dani237 <54338925+Dani237@users.noreply.github.com>
2022-12-22 22:23:30 -0500

    Feat: desktop styles added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 let btnNotify = document.querySelector('.btn');
 let emailInput = document.querySelector('.email-input');
-let callSection = document.querySelector('.call-to-action');
+let emailSection = document.querySelector('.email-content');
+let errorMessage = document.querySelector('.error-content');
 
-let errorMessage = document.createElement('div');
 errorMessage.textContent = 'Please provide a valid email address.';
 errorMessage.classList.add('div-error');
 
@@ -12,9 +12,9 @@ btnNotify.addEventListener('click', () => {
     console.log('Entrando al event click');
     if (!exreg.test(emailInput.value)) {
         emailInput.classList.add('error');
-        callSection.insertBefore(errorMessage, btnNotify);
+        errorMessage.hidden = false
     } else {
         emailInput.classList.remove('error');
-        callSection.removeChild(errorMessage);
+        errorMessage.hidden = true;
     }
 })
